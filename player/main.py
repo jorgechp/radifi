489fcd1c2c0d3e93fa4868632f70b96f2392ce27
jorgechp <jorgechp@gmail.com>
2018-11-21 23:21:02 +0100

    Add API for Alarm && Alarm management

--- player/main.py
+++ player/main.py
@@ -3,11 +3,14 @@ from music_player.music_player import Music_player
 from threading import Event, Thread
 
 from station_manager.station_manager import Station_manager
+from time_management.Alarm_manager import Alarm_manager
+
+alarm_manager = Alarm_manager()
 
 station_manager = Station_manager()
 ready_event = Event()
 music_player = Music_player(ready_event)
-api_rest = API(music_player,station_manager)
+api_rest = API(music_player,station_manager,alarm_manager)
 api_rest.start_api()
 
 # music_player.play("https://www.radioalmaina.org/radio_almaina.m3u")
