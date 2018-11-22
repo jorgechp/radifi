81b80f3a41e316b877b59cbcea98be90218e78fa
jorgechp <jorgechp@gmail.com>
2018-11-22 23:37:52 +0100

    Add cronjob systems

--- player/main.py
+++ player/main.py
@@ -5,15 +5,11 @@ from threading import Event, Thread
 from station_manager.station_manager import Station_manager
 from time_management.Alarm_manager import Alarm_manager
 
-alarm_manager = Alarm_manager()
 
+alarm_manager = Alarm_manager()
 station_manager = Station_manager()
+
 ready_event = Event()
 music_player = Music_player(ready_event)
 api_rest = API(music_player,station_manager,alarm_manager)
 api_rest.start_api()
-
-# music_player.play("https://www.radioalmaina.org/radio_almaina.m3u")
-# ready_event.wait()
-
-
