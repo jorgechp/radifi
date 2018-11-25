e837536901bfefe9f7f6869b7c4377a20db07881
jorgechp <jorgechp@gmail.com>
2018-11-25 23:47:12 +0100

    Apply PEP8 conventions

--- radifi_service/api/api.py
+++ radifi_service/api/api.py
@@ -1,35 +1,64 @@
+"""
+This module contains the APIRest of the Radifi System. It is composed with the class API, which
+handle all the request received by the APIRest Flask server.
+"""
+
 import json
 
 from flask_api import FlaskAPI
 from flask import request, Response
 
 
-class API(object):
+class API:
+    """
+    The class API represent all the entry points to Radifi service.
+    Its handle all the request in a proper format and redirect them
+    to the different manager class which conforms the Radifi service.
+    """
 
-    def __init__(self, player, station_manager,time_manager,alarm_manager):
+    def __init__(self, player_manager, station_manager, time_manager, alarm_manager):
+        """
+        Default constructor of the class.
+
+        Arguments:
+            :type player_manager: MusicPlayer
+            :param player_manager: The player subsystem
+            :type station_manager: StationManager
+            :param station_manager: The station subsystem
+            :type time_manager: TimeManager
+            :param time_manager: The Time subsystem
+            :type alarm_manager: AlarmManager
+            :param alarm_manager: The alarm subsystem
+        """
         self._app = FlaskAPI(__name__)
-        self._player = player
+        self._player_manager = player_manager
         self._station_manager = station_manager
         self._time_manager = time_manager
         self._alarm_manager = alarm_manager
 
     def start_api(self):
-        self.__define_routes()
+        """
+        Runs the FlaskAPI server, starting the APIRest servie.
+        """
+        self.__define_station_routes()
+        self.__define_alarm_routes()
+        self.__define_time_routes()
         self._app.run(debug=True)
 
-    def _handle_http_response(self, data_response, html_code = 200):
+    @staticmethod
+    def _handle_http_response(data_response: object, html_code: object = 200) -> object:
         mimetype = 'application/json'
         if data_response == "":
             return Response({}, status=html_code, mimetype=mimetype)
-        else:
-            data_as_json = json.dumps(data_response, ensure_ascii=False)
-            return Response(data_as_json, status=html_code, mimetype=mimetype)
 
-    def __define_routes(self):
+        data_as_json = json.dumps(data_response, ensure_ascii=False)
+        return Response(data_as_json, status=html_code, mimetype=mimetype)
+
+    def __define_station_routes(self):
         @self._app.route('/station/current/', methods=['GET'])
         def get_current_station():
-            current_station = self._player.get_current_station_url()
-            if current_station != None:
+            current_station = self._player_manager.get_current_station_url()
+            if current_station is not None:
                 res = {'url': current_station}
                 status = 200
             else:
@@ -37,10 +66,10 @@ class API(object):
                 status = 404
             return self._handle_http_response(res, status)
 
-        @self._app.route('/station/<int:id>', methods=['GET'])
-        def get_station_info(id):
-            if(id < len(self._station_manager.get_stations_list())):
-                station = self._station_manager.get_stations_list()[id]
+        @self._app.route('/station/<int:id_station>', methods=['GET'])
+        def get_station_info(id_station):
+            if id_station < len(self._station_manager.get_stations_list()):
+                station = self._station_manager.get_stations_list()[id_station]
                 res = {'result': station}
                 status = 200
             else:
@@ -55,24 +84,24 @@ class API(object):
 
             url = request.json['url']
             name = request.json['name']
-            id =  self._station_manager.add_station(name, url)
+            id_station = self._station_manager.add_station(name, url)
             new_station = {}
             status = 409   # Throws a HTML ERROR CODE 409
             if id > -1:
                 self._station_manager.save_stations_list()
 
                 new_station = {
-                    'id' : id,
+                    'id' : id_station,
                     'name' : name,
                     'url' : url
                 }
                 status = 200
             return self._handle_http_response(new_station, status)
 
-        @self._app.route('/station/<int:id>', methods=['DELETE'])
-        def remove_station(id):
-            if(id < len(self._station_manager.get_stations_list())):
-                station = self._station_manager.remove_station(id)
+        @self._app.route('/station/<int:id_station>', methods=['DELETE'])
+        def remove_station(id_station):
+            if id_station < len(self._station_manager.get_stations_list()):
+                station = self._station_manager.remove_station(id_station)
                 self._station_manager.save_stations_list()
                 res = {'result': station}
                 status = 200
@@ -81,12 +110,12 @@ class API(object):
                 status = 404
             return self._handle_http_response(res, status)
 
-        @self._app.route('/station/<int:id>/play', methods=['POST'])
-        def play_station(id):
-            if(id < len(self._station_manager.get_stations_list())):
-                station_url = self._station_manager.get_stations_list()[id]['url']
-                self._player.play(station_url)
-                is_played_successfully = self._player.is_played_successfully()
+        @self._app.route('/station/<int:id_station>/play', methods=['POST'])
+        def play_station(id_station):
+            if id < (len(self._station_manager.get_stations_list())):
+                station_url = self._station_manager.get_stations_list()[id_station]['url']
+                self._player_manager.play(station_url)
+                is_played_successfully = self._player_manager.is_played_successfully()
                 res = {'result': is_played_successfully}
                 status = 200
             else:
@@ -96,14 +125,16 @@ class API(object):
 
         @self._app.route('/station/stop', methods=['POST'])
         def stop_station():
-            is_deleted = self._player.stop_sound()
+            is_deleted = self._player_manager.stop_sound()
             res = {'result': is_deleted}
             return self._handle_http_response(res, 200)
 
         @self._app.route('/station/stations', methods=['GET'])
         def list_stations():
-            return Response(self._station_manager.get_stations_list(), status=200, mimetype='application/json')
+            return Response(self._station_manager.get_stations_list(),
+                            status=200, mimetype='application/json')
 
+    def __define_alarm_routes(self):
         @self._app.route('/alarm', methods=['GET'])
         def get_current_alarm():
             current_alarm = self._alarm_manager.get_current_alarm()
@@ -143,18 +174,19 @@ class API(object):
             else:
                 is_default_song = request.args.get('defaultSong')
             is_default_song_as_boolean = bool(is_default_song)
-            self._player.play_alarm(is_default_song_as_boolean)
-            is_played = self._player.is_played_successfully()
+            self._player_manager.play_alarm(is_default_song_as_boolean)
+            is_played = self._player_manager.is_played_successfully()
             if is_played:
                 return self._handle_http_response("", 204)
-            else:
-                return self._handle_http_response("", 406)
+
+            return self._handle_http_response("", 406)
 
         @self._app.route('/alarm/stop', methods=['GET'])
         def stop_alarm():
-            self._player.stop_player()
+            self._player_manager.stop_player()
             return self._handle_http_response("", 204)
 
+    def __define_time_routes(self):
         @self._app.route('/time', methods=['GET'])
         def get_current_time():
             current_time = self._time_manager.get_current_time()
