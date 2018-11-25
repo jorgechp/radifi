adb92c9cd4d838d7c9c00cb38cd5d98a1dadb765
jorgechp <jorgechp@gmail.com>
2018-11-25 23:06:49 +0100

    Apply PEP8 conventions

---
+++ radifi_service/music/music_player.py
@@ -0,0 +1,96 @@
+import configparser
+import requests
+import vlc
+import os
+import time
+
+from threading import Event, Thread
+
+CONFIG_FILE_URL = 'config/radifi_configuration.ini'
+
+class MusicPlayer(object):
+
+    def __init__(self,ready_event = None):
+        """
+        Default constructor
+        """
+        self._config = configparser.ConfigParser()
+        self._config.read(CONFIG_FILE_URL)
+        self._vlc_instance = vlc.Instance()
+        self._ready = ready_event
+        self._url = None
+        self._player = None
+        self._is_played_succesfully = False
+
+    def is_played_successfully(self):
+        return self._is_played_succesfully
+
+    def is_playing(self):
+        return self._player != None
+
+    def play(self,url):
+        self._url = url
+        self._play_sound()
+
+    def _play_sound(self):
+        """
+        Play the url specified. It can be a remote url or a local file.
+        """
+
+        if self.is_playing():
+            self._is_played_succesfully = False
+            return
+
+        is_local_file = self._url[:4] == 'file'
+        if (is_local_file):
+            is_found = os.path.isfile(self._url[7:])
+        else:
+            remote_request = requests.get(self._url, stream=True)
+            is_found = remote_request.ok
+
+        if(is_found):
+            media_list = self._vlc_instance.media_list_new([self._url])
+            if(is_local_file):
+                self._player = self._vlc_instance.media_player_new()
+                media_to_set = self._vlc_instance.media_new(self._url)
+                self._player.set_media(media_to_set)
+            else:
+                self._player = self._vlc_instance.media_list_player_new()
+                self._player.set_media_list(media_list)
+
+            self._player.play()
+            time.sleep(0.5)
+            self._is_played_succesfully = True
+        else:
+            self._error_code = 2
+            self._is_played_succesfully = False
+
+
+    def play_alarm(self, is_default_song = 0):
+        alarm_configuration = self._config['ALARM']
+        url_to_play = alarm_configuration['default_station_url']
+        if(is_default_song or len(url_to_play) == 0):
+            abs_path = os.path.abspath(alarm_configuration['default_alarm_path'])
+            url_to_play = "file://" + abs_path
+        self.play(url_to_play)
+
+
+    def stop_player(self):
+        self._player.stop()
+        self._player = None
+
+    def stop_sound(self):
+        if(self._player != None):
+            self._player.stop()
+            self._ready.set()
+            self._player = None
+            return True
+        else:
+            return False
+
+
+    def get_thread(self):
+        return self.__thread
+
+    def get_current_station_url(self):
+        return self._url
---
+++ radifi_service/time/alarm_manager.py
@@ -0,0 +1,66 @@
+import threading
+import time
+
+import schedule
+
+from scripts import launch_alarm_script
+
+
+class AlarmManager(object):
+
+    def __init__(self,config):
+        self._scheduler_thread = None
+        self._config = config
+        self._config_alarm = self._config.get_properties_group('ALARM')
+        self.set_current_alarm(self._config_alarm['alarm_hour'], self._config_alarm['alarm_minute'])
+        self.toggle_alarm(self._config_alarm.getboolean('alarm_enabled'))
+
+    def get_current_alarm(self):
+        return self._hour_to_set, self._minute_to_set
+
+    def set_current_alarm(self, hour_to_set,minute_to_set):
+        self._hour_to_set = hour_to_set
+        self._minute_to_set = minute_to_set
+
+    def is_alarm_enabled(self):
+        return self.__is_alarm_enabled()
+
+    def _clear_scheduler(self):
+        self._scheduler_thread.clear()
+        self._scheduler_thread = None
+        schedule.clear()
+
+    def _trigger_scheduler(self):
+        scheduler_thread = threading.Event()
+        class ScheduleThread(threading.Thread):
+            @classmethod
+            def run(cls):
+                while not scheduler_thread.is_set():
+                    schedule.run_pending()
+                    time.sleep(2)
+
+        continuous_thread = ScheduleThread()
+        continuous_thread.start()
+        self._scheduler_thread = scheduler_thread
+
+    def _enable_alarm(self):
+        time_parsed = str(self._hour_to_set) + ":" + str(self._minute_to_set)
+        schedule.every().day.at(time_parsed).do(AlarmManager.execute_alarm)
+        self._trigger_scheduler()
+
+
+    def toggle_alarm(self, is_alarm_enabled):
+        self.__is_alarm_enabled = is_alarm_enabled
+        if self._scheduler_thread != None:
+            self._clear_scheduler()
+        if is_alarm_enabled:
+            self._enable_alarm()
+
+    def save_status(self):
+        self._config.save()
+
+    def execute_alarm(self):
+        general_config = self._config.get_properties_group('GENERAL')
+        url_to_call = general_config['base_url']
+        port = general_config['port']
+        launch_alarm_script.execute(url_to_call, port)
