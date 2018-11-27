332b59d24128538df14bb54e1935d5be3d624dbe
jorchamo <jorge.chamorro@t-systems.com>
2018-11-27 16:09:45 +0100

    Add setup.py and update requirements.txt

---
+++ radifi_service/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='Radifi',
+    version='0.1.0',
+    author='Jorge Chamorro-Padial',
+    author_email='jorge@jorgechp.com',
+    packages=['api', 'config', 'music', 'planning', 'resources', 'scripts' , 'station', 'tests'],
+    scripts=['main.py'],
+    license='LICENSE',
+    description='Your own DIY internet radio service.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "python-vlc >= 3.0.4106",
+        "requests == 2.20.1",
+        "Flask-API == 1.0",
+        "flask == 1.0.2",
+        "validators == 0.12.3",
+        "schedule == 0.5.0"
+    ],
+)
