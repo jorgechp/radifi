13651b5c2747aae92efa238a223f0d26ae487c05
jorgechp <jorgechp@yandex.com>
2020-09-06 00:58:14 +0200

    Add entry point to update the system time

--- radifi_service/planning/time_manager.py
+++ radifi_service/planning/time_manager.py
@@ -9,6 +9,7 @@ This module manages the Radifi Time System to perform the following operations:
 import datetime
 import os
 import subprocess
+import sys
 
 
 def check_root_privileges():
@@ -68,3 +69,68 @@ class TimeManager:
         ntp_server = self._config_time['server']
         os.environ['TZ'] = time_zone
         os.system("ntpdate -s  " + ntp_server)
+
+    def _set_system_time_linux(self, time_tuple: tuple):
+        """
+        Updates the system time on a Linux OS.
+        """
+
+        import ctypes.util
+        import time
+
+        # /usr/include/linux/time.h:
+        #
+        # define CLOCK_REALTIME                     0
+        CLOCK_REALTIME = 0
+
+        # /usr/include/time.h
+        #
+        # struct timespec
+        #  {
+        #    __time_t tv_sec;            /* Seconds.  */
+        #    long int tv_nsec;           /* Nanoseconds.  */
+        #  };
+        class timespec(ctypes.Structure):
+            _fields_ = [("tv_sec", ctypes.c_long),
+                        ("tv_nsec", ctypes.c_long)]
+
+        librt = ctypes.CDLL(ctypes.util.find_library("rt"))
+
+        ts = timespec()
+        ts.tv_sec = int(time.mktime(datetime.datetime(*time_tuple[:6]).timetuple()))
+        ts.tv_nsec = time_tuple[6] * 1000000  # Millisecond to nanosecond
+
+        # http://linux.die.net/man/3/clock_settime
+        librt.clock_settime(CLOCK_REALTIME, ctypes.byref(ts))
+
+    def _set_system_time_windows(self, time_tuple: tuple):
+        """
+        Updates the system time on a Windows OS.
+        """
+
+        import pywin32
+        # http://timgolden.me.uk/pywin32-docs/win32api__SetSystemTime_meth.html
+        # pywin32.SetSystemTime(year, month , dayOfWeek , day , hour , minute , second , millseconds )
+        dayOfWeek = datetime.datetime(time_tuple).isocalendar()[2]
+        pywin32.SetSystemTime(time_tuple[:2] + (dayOfWeek,) + time_tuple[2:])
+
+    def set_system_time(self, year: int, month: int, day: int, hour: int, minute: int) -> None:
+        """
+        Updates the system time. It's implemented for Linux and Windows.
+        """
+
+        time_tuple = (year,  # Year
+                      month,  # Month
+                      day,  # Day
+                      hour,  # Hour
+                      minute,  # Minute
+                      0,  # Second
+                      0,  # Millisecond
+                      )
+        if sys.platform == 'linux2':
+            self._set_system_time_linux(time_tuple)
+        elif sys.platform=='win32':
+            self._set_system_time_windows(time_tuple)
+
+
+
